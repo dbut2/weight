f5c4f7aa33bf0a593402976475d154a16e4aa722
Dylan Butler <dylanbutler@outlook.com.au>
2023-09-19 19:46:29 +1000

    verification

--- go.mod
+++ go.mod
@@ -2,19 +2,31 @@ module github.com/dbut2/weight
 
 go 1.21
 
-require github.com/Thomas2500/go-fitbit v1.2.3
+require (
+	cloud.google.com/go/datastore v1.14.0
+	github.com/Thomas2500/go-fitbit v1.2.3
+	github.com/gin-gonic/gin v1.9.1
+	golang.org/x/oauth2 v0.12.0
+)
 
 require (
+	cloud.google.com/go v0.110.7 // indirect
+	cloud.google.com/go/compute v1.23.0 // indirect
+	cloud.google.com/go/compute/metadata v0.2.3 // indirect
 	github.com/bytedance/sonic v1.9.1 // indirect
 	github.com/chenzhuoyu/base64x v0.0.0-20221115062448-fe3a3abad311 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.2 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
-	github.com/gin-gonic/gin v1.9.1 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.14.0 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/google/go-cmp v0.5.9 // indirect
+	github.com/google/s2a-go v0.1.4 // indirect
+	github.com/googleapis/enterprise-certificate-proxy v0.2.4 // indirect
+	github.com/googleapis/gax-go/v2 v2.12.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.4 // indirect
 	github.com/leodido/go-urn v1.2.4 // indirect
@@ -24,13 +36,20 @@ require (
 	github.com/pelletier/go-toml/v2 v2.0.8 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.11 // indirect
+	go.opencensus.io v0.24.0 // indirect
 	golang.org/x/arch v0.3.0 // indirect
-	golang.org/x/crypto v0.12.0 // indirect
-	golang.org/x/net v0.14.0 // indirect
-	golang.org/x/oauth2 v0.11.0 // indirect
-	golang.org/x/sys v0.11.0 // indirect
-	golang.org/x/text v0.12.0 // indirect
+	golang.org/x/crypto v0.13.0 // indirect
+	golang.org/x/net v0.15.0 // indirect
+	golang.org/x/sync v0.2.0 // indirect
+	golang.org/x/sys v0.12.0 // indirect
+	golang.org/x/text v0.13.0 // indirect
+	golang.org/x/xerrors v0.0.0-20220907171357-04be3eba64a2 // indirect
+	google.golang.org/api v0.128.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/genproto v0.0.0-20230821184602-ccc8af3d0e93 // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20230803162519-f966b187b2e5 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20230803162519-f966b187b2e5 // indirect
+	google.golang.org/grpc v1.57.0 // indirect
 	google.golang.org/protobuf v1.31.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -1,36 +1,184 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
+	"errors"
 	"fmt"
 	"net/http"
 	"os"
 
+	"cloud.google.com/go/datastore"
+	"github.com/Thomas2500/go-fitbit"
 	"github.com/gin-gonic/gin"
+	"golang.org/x/oauth2"
 )
 
+type weight struct {
+	Date        string
+	FitbitLogID int64
+	Time        string
+	Weight      float64
+}
+
+var dsc *datastore.Client
+var fc *fitbit.Session
+
 func main() {
+	setupConfiguration()
+	e := setupRoutes()
+	startServer(e)
+}
+
+func setupConfiguration() {
+	var err error
+	dsc, err = datastore.NewClient(context.Background(), os.Getenv("PROJECT_ID"))
+	if err != nil {
+		panic(err)
+	}
+
+	token := &oauth2.Token{}
+	err = json.Unmarshal([]byte(os.Getenv("FITBIT_TOKEN")), token)
+	if err != nil {
+		panic(err)
+	}
+
+	fc = fitbit.New(fitbit.Config{
+		ClientID:     os.Getenv("FITBIT_CLIENT"),
+		ClientSecret: os.Getenv("FITBIT_SECRET"),
+		RedirectURL:  os.Getenv("FITBIT_REDIRECT"),
+		Scopes:       []fitbit.Scope{fitbit.ScopeWeight},
+		Locale:       "en-au",
+	})
+	fc.SetToken(token)
+}
+
+func setupRoutes() *gin.Engine {
+	e := gin.Default()
+
+	e.POST("/receive", receivePostHandler)
+	e.GET("/receive", receiveGetHandler)
+	e.GET("/", rootHandler)
+
+	return e
+}
+
+func startServer(e *gin.Engine) {
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 	}
-
-	e := gin.Default()
-
-	e.POST("/receive", func(c *gin.Context) {
-		data := map[string]any{}
-		err := c.BindJSON(&data)
-		if err != nil {
-			_ = c.Error(err)
-			c.Status(http.StatusInternalServerError)
-			return
-		}
-		fmt.Println(data)
-	})
-
-	e.GET("/")
-
 	err := http.ListenAndServe(":"+port, e)
 	if err != nil {
-		panic(err.Error())
+		panic(err)
 	}
+}
+
+func handleError(c *gin.Context, err error) {
+	_ = c.Error(err)
+	c.Status(http.StatusInternalServerError)
+}
+
+func receivePostHandler(c *gin.Context) {
+	var subs []fitbit.Subscription
+	err := c.BindJSON(&subs)
+	if err != nil {
+		handleError(c, err)
+		return
+	}
+
+	if len(subs) == 0 {
+		handleError(c, errors.New("no subscriptions"))
+		return
+	}
+
+	sub := subs[0]
+
+	bw, err := fc.BodyWeightLogByDay(sub.Date)
+	if err != nil {
+		handleError(c, err)
+		return
+	}
+
+	var existingWeights []weight
+	keys, err := dsc.GetAll(c, datastore.NewQuery("Weight").FilterField("Date", "=", sub.Date), &existingWeights)
+	if err != nil {
+		handleError(c, err)
+		return
+	}
+
+	for i, existingWeight := range existingWeights {
+		found := false
+		for _, w := range bw.Weight {
+			if existingWeight.FitbitLogID == w.LogID {
+				found = true
+				break
+			}
+		}
+		if found {
+			continue
+		}
+
+		err = dsc.Delete(c, keys[i])
+		if err != nil {
+			handleError(c, err)
+			return
+		}
+	}
+
+	for _, w := range bw.Weight {
+		found := false
+		for _, existingWeight := range existingWeights {
+			if w.LogID == existingWeight.FitbitLogID {
+				found = true
+				break
+			}
+		}
+		if found {
+			continue
+		}
+
+		ww := weight{
+			Date:        w.Date,
+			FitbitLogID: w.LogID,
+			Time:        w.Time,
+			Weight:      w.Weight,
+		}
+
+		_, err = dsc.Put(c, datastore.IDKey("Weight", ww.FitbitLogID, nil), &ww)
+		if err != nil {
+			handleError(c, err)
+			return
+		}
+	}
+}
+
+func receiveGetHandler(c *gin.Context) {
+	verification := os.Getenv("FITBIT_VERIFICATION")
+	verify := c.Query("verify")
+
+	if verify != verification {
+		c.Status(http.StatusNotFound)
+		return
+	}
+
+	c.Status(http.StatusNoContent)
+}
+
+func rootHandler(c *gin.Context) {
+	var weights []weight
+	_, err := dsc.GetAll(c, datastore.NewQuery("Weight").Order("-Time"), &weights)
+	if err != nil {
+		_ = c.Error(err)
+		c.Status(http.StatusInternalServerError)
+		return
+	}
+
+	if len(weights) == 0 {
+		_ = c.Error(errors.New("no weights"))
+		c.Status(http.StatusInternalServerError)
+		return
+	}
+
+	c.String(http.StatusOK, fmt.Sprintf("%f", weights[0].Weight))
 }
